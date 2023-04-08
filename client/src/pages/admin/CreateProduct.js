aa88da225dc576edb89bed39d7c6815cea1157fd
Devesh Kashyap <devesh.pe@gmail.com>
2023-04-08 19:32:32 +0530

    Created products Category front-end

--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -5,9 +5,11 @@ import toast from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { Select } from "antd";
-const { option } = Select;
+import { useNavigate } from "react-router-dom";
+const { Option } = Select;
 
 const CreateProduct = () => {
+  const navigate = useNavigate() ;
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -24,7 +26,7 @@ const CreateProduct = () => {
         `${process.env.REACT_APP_API}/api/v1/category/get-category`
       );
       if (data?.success) {
-        setCategories(data.category);
+        setCategories(data?.category);
       }
     } catch (error) {
       console.log(error);
@@ -35,6 +37,31 @@ const CreateProduct = () => {
   useEffect(() => {
     getAllCategory();
   }, []);
+
+  //create product
+  const handleCreate = async(e) => {
+    e.preventDefault()
+    try{
+      const productData = new FormData()
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("quantity", quantity);
+      productData.append("photo", photo);
+      productData.append("category", category);
+      const {data} = axios.post(`${process.env.REACT_APP_API}/api/v1/product/create-product`, productData )
+      if (data?.success){
+        toast.error(data?.message)
+      }else {
+        toast.success('Product Created Successfully')
+        navigate('/dashboard/admin/products')
+      }
+    }catch(error){
+      console.log(error)
+      toast.error('Something went wrong')
+    }
+  }
+
   return (
     <Layout title={"Dashboard - Create Product"}>
       <div className="container-fluid m-3 p-3">
@@ -51,18 +78,17 @@ const CreateProduct = () => {
                 size="large"
                 showSearch
                 className="form-select mb-3"
-                onChange={(value) => setCategory(value)}
-              >
-                {categories?.map((c) => (
-                  <option key={c._id} value={c.name}>
+                onChange={(value) => { setCategory(value) }} >
+                {categories?.map(c => (
+                  <Option key={c._id} value={c._id}>
                     {c.name}
-                  </option>
+                  </Option>
                 ))}
               </Select>
               <div className="mb-3">
                 <label
                   className="btn btn-outline-secondary col-md-12">
-                  {photo? photo.name:"Upload Photo"}
+                  {photo ? photo.name : "Upload Photo"}
                   <input
                     type="file"
                     name="photo"
@@ -75,11 +101,70 @@ const CreateProduct = () => {
               <div className="mb-3">
                 {photo && (
                   <div className="text-center">
-                     <img src={URL.createObjectURL(photo)} alt="product_photo"  height={"200px"} className="img img-responsive"/>
+                    <img src={URL.createObjectURL(photo)} alt="product_photo" height={"200px"} className="img img-responsive" />
 
-                    </div>)}
+                  </div>
+                )}
               </div>
-              //bhargab continue from here
+
+              <div className="mb-3">
+                <input type="text"
+                  value={name}
+                  placeholder="write a name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  placeholder="write a description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="write a Price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="write a quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <Select
+                  bordered={false}
+                  placeholder="Select Shipping "
+                  size="large"
+                  showSearch
+                  className="form-select mb-3"
+                  onChange={(value) => {
+                    setShipping(value);
+                  }}
+                >
+                  <Option value="0">No</Option>
+                  <Option value="1">Yes</Option>
+                </Select>
+              </div>
+              <div className="mb-3">
+                <button className="btn btn-primary" onClick={handleCreate}>
+                  CREATE PRODUCT
+                </button>
+              </div>
+
             </div>
           </div>
         </div>
