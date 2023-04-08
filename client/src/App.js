714a68b524c5e4739e7844fcce9842cb48da2188
Mohit Gupta <mohitgupta4357@gmail.com>
2023-04-08 21:18:38 +0530

    added admin product update functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ import CreateProduct from "./pages/admin/CreateProduct.js";
 import Users from "./pages/admin/Users.js";
 import Orders from "./pages/user/Order.js";
 import Profile from "./pages/user/Profile.js";
+import Products from "./pages/admin/Product.js";
+import UpdateProduct from "./pages/admin/UpdateProduct.js";
 
 function App() {
   return (
@@ -33,6 +35,8 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
         </Route>
 
