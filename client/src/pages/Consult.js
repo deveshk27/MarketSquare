9dcc0b0176266577c0ecd3dbed06b6e40d921694
Mohit Gupta <mohitgupta4357@gmail.com>
2023-04-08 16:24:29 +0530

    minot change

--- client/src/pages/Consult.js
+++ client/src/pages/Consult.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useState } from "react";
 import Layout from "./../components/Layout/Layout";
 import '../App.css'
 import { Form, Button } from 'react-bootstrap';
@@ -9,32 +8,61 @@ import toast from 'react-hot-toast';
 
 
 const Consult = () => {
-    const [prompt, setPrompt] = useState(" ");
-    const [response, setResponse] = useState(" ");
-    async function handleButtonClicked() {
-        const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/chat`, { prompt });
-        setResponse(res.data);
-    }
+    const [prompt, setPrompt] = useState("");
+    const [response, setResponse] = useState("");
+    const navigate = useNavigate();
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleButtonClicked();
+        }
+    };
+
+    const handleButtonClicked = async () => {
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/chat`, { prompt });
+            setResponse(res.data);
+        } catch (error) {
+            console.error(error);
+            toast.error("An error occurred. Please try again.");
+        }
+    };
+
     return (
         <Layout>
-            {/* <div class="containerworking"> */}
-                <h1 className="title">Consult About Your Pet</h1>
-
-                <Form className="consultancy">
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label className="headers">Question</Form.Label>
-                        <Form.Control className="form-control12" type="text" placeholder="Enter your question here" value={prompt} onChange={(data) => setPrompt(data.target.value)} required />
-                    </Form.Group>
-                    <Button onClick={handleButtonClicked} variant="secondary" size='lg'>
-                        Ask
-                    </Button>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className="headers">Answer</Form.Label>
-                        <Form.Control className="form-control12"  as="textarea" rows={10} placeholder={response} />
-                    </Form.Group>
-                </Form>
-            {/* </div> */}
-
+            <h1 className="title">Consult About Your Pet</h1>
+            <Form className="consultancy">
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label className="headers">Question</Form.Label>
+                    <Form.Control
+                        className="form-control12"
+                        type="text"
+                        placeholder="Enter your question here"
+                        value={prompt}
+                        onChange={(event) => setPrompt(event.target.value)}
+                        onKeyDown={handleKeyDown}
+                        required
+                    />
+                </Form.Group>
+                <Button
+                    variant="secondary"
+                    size='lg'
+                    onClick={handleButtonClicked}
+                >
+                    Ask
+                </Button>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label className="headers">Answer</Form.Label>
+                    <Form.Control
+                        className="form-control12"
+                        as="textarea"
+                        rows={10}
+                        placeholder={response}
+                        readOnly
+                    />
+                </Form.Group>
+            </Form>
         </Layout>
     );
 };
--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./../../components/Layout/Layout";
 import AdminMenu from "./../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Select } from "antd";
+const { option } = Select;
 
 const CreateProduct = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  //getAllCategory
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/category/get-category`
+      );
+      if (data?.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title={"Dashboard - Create Product"}>
       <div className="container-fluid m-3 p-3">
@@ -12,6 +44,43 @@ const CreateProduct = () => {
           </div>
           <div className="col-md-9">
             <h1>Create Product</h1>
+            <div className="m-1 w-75">
+              <Select
+                bordered={false}
+                placeholder="Select a category"
+                size="large"
+                showSearch
+                className="form-select mb-3"
+                onChange={(value) => setCategory(value)}
+              >
+                {categories?.map((c) => (
+                  <option key={c._id} value={c.name}>
+                    {c.name}
+                  </option>
+                ))}
+              </Select>
+              <div className="mb-3">
+                <label
+                  className="btn btn-outline-secondary col-md-12">
+                  {photo? photo.name:"Upload Photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => setPhoto(e.target.files[0])}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                     <img src={URL.createObjectURL(photo)} alt="product_photo"  height={"200px"} className="img img-responsive"/>
+
+                    </div>)}
+              </div>
+              //bhargab continue from here
+            </div>
           </div>
         </div>
       </div>
